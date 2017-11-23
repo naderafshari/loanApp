33855712817f09c9abe674516491ed200e0d7682
nader <nader.afshari.usa@gmail.com>
2017-11-23 13:57:15 -0800

    fix google login

--- src/app/component/user-profile/user-profile.component.ts
+++ src/app/component/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../provider/auth.service';
 import { Router } from '@angular/router';   
+import { subscribeOn } from 'rxjs/operator/subscribeOn';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,7 +11,11 @@ import { Router } from '@angular/router';
 export class UserProfileComponent implements OnInit {
 
   constructor(public authService: AuthService, private router:Router) { 
-    console.log(authService.user);
+    let user = authService.user.subscribe(
+      //google login would not have the user updated when redirected to this page
+      //when subscribing to the user is updated when observer is fired 
+      //() => console.log("observer fired")
+    );
   }
 
   ngOnInit() {
@@ -21,7 +26,7 @@ export class UserProfileComponent implements OnInit {
     .then(
       () => {
               //alert('User logged out successfully!');
-              this.router.navigateByUrl('/email-login');
+              this.router.navigateByUrl('/login');
       },
 //    this.email = this.password = '',
       err => alert(err)
