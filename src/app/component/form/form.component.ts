b45e5fa405e02ebe5fde373a9226c79b050e2a49
naderafshari <nader.afshari.usa@gmail.com>
2017-12-25 11:40:34 -0800

    switching to other computer

--- src/app/component/form/form.component.ts
+++ src/app/component/form/form.component.ts
@@ -22,7 +22,24 @@ export class FormComponent implements OnInit {
   formId: string;
   formData: Form;
   formRef: AngularFirestoreCollection<Form>;
-
+  fields: {
+    index: number;
+    name: string;
+    type: string;
+    //option1: string;
+    value: string;
+  }[] = [];
+  /*names : string[] = [];
+  types : string[] = [];
+  option1 : string[] = [];
+  option2 : string[] = [];
+  option3 : string[] = [];
+  option4 : string[] = [];
+  option5 : string[] = [];
+  option6 : string[] = [];
+  values : string[] = [];*/
+  index = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  
   constructor(private afs: AngularFirestore,
     public authService: AuthService,
     private router: Router,
@@ -38,10 +55,42 @@ export class FormComponent implements OnInit {
           ref.where('formId', '==', this.formId).orderBy('startTime', 'desc').limit(1));
         this.forms = this.formRef.valueChanges();
       }
-      this.forms.subscribe((data) => this.formData = data[0]);
-    });
+      this.forms.subscribe((data) => {
+        this.formData = data[0];
+        //if(this.formData.field1 !== undefined) {
+          for (let i = 1; i <= 20; i++) {
+            const obj: Form = this.formData;
+            this.fields.push({
+                                index: i,
+                                name: eval('obj.field'+i+'.name'), 
+                                type: eval('obj.field'+i+'.type'), 
+                                value: eval('obj.field'+i+'.value')
+                              });
+            /*this.types.push(eval('obj.field'+i+'.type'));
+            this.option1.push(eval('obj.field'+i+'.option1'));
+            this.option2.push(eval('obj.field'+i+'.option2'));
+            this.option3.push(eval('obj.field'+i+'.option3'));
+            this.option4.push(eval('obj.field'+i+'.option4'));
+            this.option5.push(eval('obj.field'+i+'.option5'));
+            this.option6.push(eval('obj.field'+i+'.option6'));
+            this.values.push('formData.field'+i+'.value');*/
+          }
+        //}
+        console.log(this.fields);
+       });
+  });
+  }
+  getValue(i){
+    return this.fields[i].value;
   }
 
+  setValue(value,i) {
+    console.log(value);
+    console.log(i)
+    //const obj: Form = this.formData;
+    //eval('obj.field'+i+'.value = '+value);
+    //this.formData = obj;
+  }
   updateFormData() {
     if (this.formData != null ) {
       this.formData.updateTime = new Date().toString();
